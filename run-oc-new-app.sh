db4a2e5357415ac92fa3891946d7bd0aeae3335f
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-10-05 13:49:31 -0600

    Update README.md

--- run-oc-new-app.sh
+++ run-oc-new-app.sh
@@ -1,6 +1,7 @@
-# Sample Demo app for OpenShift
+#!/usr/bin/env
 
-```bash
+# Additional ags:  --source-secret if repo is private
+
 oc new-project basic-ocp-demo-development
 # use branch "development"
 oc new-app https://github.com/FreedomBen/basic-ocp-demo#development
@@ -10,4 +11,3 @@ oc new-project basic-ocp-demo-production
 # use branch "production"
 oc new-app https://github.com/FreedomBen/basic-ocp-demo#production
 oc expose svc basic-ocp-demo
-```
